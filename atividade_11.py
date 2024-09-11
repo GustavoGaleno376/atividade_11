c45464a284316959c2a4b6268e2468f7e14b4e1d
GustavoGaleno376 <gustabrffmano@gmail.com>
2024-09-11 18:28:13 -0300

    idade e faixa etária

---
+++ atividade_11.py
@@ -0,0 +1,24 @@
+# Crie um programa que receba a idade de uma pessoa e classifique-a de acordo com as seguintes faixas etárias:
+idade=int(input("qual é sua idade"))
+# Criança (0-12 anos)
+if (idade<=12):
+    print("você é uma criança")
+
+# Adolescente (13-17 anos)
+elif (idade<= 17):
+    print("você é adolescente")
+
+# Adulto (18-59 anos)
+
+elif (idade<=18):
+    print("você é adulto")
+
+elif (idade<=59):
+     print("você é adulto")
+
+
+#Idoso (60 anos ou mais)
+elif (idade>=60 ):
+    print("você é idoso")
+elif(idade>60):
+    print=("você é idoso")
